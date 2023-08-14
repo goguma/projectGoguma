882e2a0e5ba9796e54642dfb8ede1e36831235fb
SeongWon Cho <seongwon79@hanafos.com>
2023-08-14 23:42:16 +0900

    basic commit
    1. You need to migration.
    2. prepare postgresql
    
    Signed-off-by: SeongWon Cho <seongwon79@hanafos.com>

---
+++ projectGoguma/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-x1ffogq@81y29#oceam97-!s#+ndyug3gj+n!q3ww9@*w77jxg"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
